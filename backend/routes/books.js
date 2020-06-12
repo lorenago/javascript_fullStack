95cf437b82cd25e9783f4dbd38decc8f2a08fbb8
Lorena González <lore_383@hotmail.com>
2020-06-12 23:57:54 +0200

    feat(book): API for books. List, delete and create books.

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,28 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) =>  res.json({ text: 'Hello world' }));
+const Book = require('../models/Book');
+
+router.get('/', async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+});
+
+router.post('/', async (req, res) => {
+    const { title, author, isbn } = req.body;
+    const newBook = new Book({ title, author, isbn });
+    await newBook.save();
+    // res.send('Book saved');
+    res.json({ message: 'Book saved' });
+});
+
+router.delete('/:id', async (req, res) => {
+    // console.log(req.params.id);
+    // const book = await Book.findByIdAndDelete(req.params.id);
+    // console.log(book);
+    // res.send('Deleting');
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Book deleted' });
+});
 
 module.exports = router;
